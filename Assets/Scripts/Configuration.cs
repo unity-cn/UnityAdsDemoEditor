f0c4957b293aae9d4cca95e793a15b86785717b2
Callie Zhu <zhujiali@unity3d.com>
2017-09-18 13:44:13 +0800

    Showcase integrate Unity Ads in Unity Editor by package. Unity编辑器通过Unity Ads SDK Package集成Unity Ads服务的示例。

---
+++ Assets/Scripts/Configuration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Configuration {
+
+	/// <summary>
+	/// Game ID
+	/// </summary>
+	public static string snakeGameId = "1210390";
+	// android, useless
+	public static string crossyRoadGameId = "131624390"; 
+	// project name : Unity Ads Demo Editor
+	public static string myGameIdiOS = "1544006"; 
+	public static string myGameIdAndroid = "1544208"; 
+	public static string tempGameId = "1394428";
+
+	/// <summary>
+	/// Replacement ID
+	/// </summary>
+	public static string skippablePlacementId = "video";
+	public static string unskippablePlacementId = "rewardedVideo";
+	public static string unskippablePlacementId2 = "rewardedVideoZone";
+	public static string longTimeSkippablePlacementId = "defaultVideoAndPictureZone";
+
+	/// <summary>
+	/// Video Stats
+	/// </summary>
+	public static string videoCompleted = "Video completed - Offer a reward to the player";
+	public static string videoSkipped = "Video was skipped - Do NOT reward the player";
+	public static string videoError = "Video failed to show";
+	public static string videoAvailable = "Get free coins";
+	public static string videoUnavailable = "Free coins not available yet";
+
+	/// <summary>
+	/// The rewarded coin amount.
+	/// </summary>
+	public static int rewardedCoinAmount = 7;
+
+}
---
+++ Assets/Scripts/TestUnityAds.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+
+[RequireComponent(typeof(Button))]
+public class TestUnityAds : MonoBehaviour {
+
+	ColorBlock newColorBlock = new ColorBlock();
+	public Color green = new Color(0.1F, 0.8F, 0.1F, 1.0F);
+
+
+	//---------- ONLY NECESSARY FOR ASSET PACKAGE INTEGRATION: ----------//
+
+	#if UNITY_IOS
+	private string gameId = Configuration.myGameIdiOS;
+	#elif UNITY_ANDROID
+	private string gameId = Cofiguration.myGameIdAndroid;
+	#endif
+
+	//-------------------------------------------------------------------//
+
+	// 观看视频广告后获得的硬币奖励
+	public Text m_CoinText;
+	// Game ID
+	public Text m_GameIDText;
+	// Replacement ID
+	public Text m_ReplacementIDText;
+	// Stats输出信息
+	public Text m_StatsText;
+	// 奖励视频按钮提示
+	public Text m_ButtonText;
+
+	// 奖励视频按钮
+	Button m_Button;
+	// 奖励硬币
+	int m_RewardedCoin = 0;
+	// 奖励视频是否可用
+	bool m_IsVideoAvailable = false;
+
+	public string placementId = "rewardedVideo";
+
+	void Start ()
+	{  
+		m_CoinText.text = m_RewardedCoin.ToString ();
+		m_GameIDText.text = Configuration.myGameIdiOS;
+		m_ReplacementIDText.text = Configuration.unskippablePlacementId;
+		m_StatsText.gameObject.SetActive(false);
+
+		m_Button = GetComponent<Button>();
+		if (m_Button) m_Button.onClick.AddListener(ShowAd);
+
+		if (Advertisement.isSupported) {
+			Advertisement.Initialize (gameId, true);
+		}
+
+	}
+
+	void Update ()
+	{
+		if (m_Button) {
+			m_IsVideoAvailable = Advertisement.IsReady(placementId);
+
+			m_Button.interactable = m_IsVideoAvailable;
+			m_ButtonText.text = m_IsVideoAvailable ? Configuration.videoAvailable : Configuration.videoUnavailable; 
+		}
+	}
+
+	void ShowAd ()
+	{
+		var options = new ShowOptions();
+		options.resultCallback = HandleShowResult;
+
+		Advertisement.Show(placementId, options);
+	}
+
+	void RewardPlayer()
+	{
+		m_RewardedCoin += Configuration.rewardedCoinAmount;
+		m_CoinText.text = m_RewardedCoin.ToString ();
+	}
+
+	void HandleShowResult (ShowResult result)
+	{
+		m_StatsText.gameObject.SetActive(true);
+		if(result == ShowResult.Finished) {
+			RewardPlayer ();
+			m_StatsText.text = Configuration.videoCompleted;
+
+		}else if(result == ShowResult.Skipped) {
+
+			m_StatsText.text = Configuration.videoSkipped;
+
+		}else if(result == ShowResult.Failed) {
+			m_StatsText.text = Configuration.videoError;
+		}
+	}
+}
